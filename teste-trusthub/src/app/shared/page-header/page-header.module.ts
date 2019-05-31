b76119168364156f6574f20c9a6c7820732f069f
Gabriel Aguiar Pineiro <gabriel.silva@iteris.com.br>
2019-05-30 22:59:36 -0300

    correções pacotes travando npm install

--- teste-trusthub/src/app/shared/page-header/page-header.module.ts
+++ teste-trusthub/src/app/shared/page-header/page-header.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageHeaderComponent } from './page-header.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 
 @NgModule({
   imports: [
     CommonModule,
-    FontAwesomeModule,
     FormsModule,
     NgSelectModule
   ],
